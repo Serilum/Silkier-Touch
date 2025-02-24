5528e9adf1570f75498e4a71010b341f922e5628
Rick South <rick@natamus.com>
2025-02-24 13:08:21 +0100

    Silkier Touch source code for 1.21.1.

---
+++ Forge/src/main/java/com/natamus/silkiertouch/forge/events/ForgeBlockEventss.java
@@ -0,0 +1,36 @@
+package com.natamus.silkiertouch.forge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.silkiertouch.events.BlockEvents;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ForgeBlockEventss {
+	@SubscribeEvent
+	public static void onBlockBreak(BlockEvent.BreakEvent e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+		
+		BlockEvents.onBlockBreak(level, e.getPlayer(), e.getPos(), e.getState(), null);
+	}
+
+	@SubscribeEvent
+	public static void onEntityBlockPlace(BlockEvent.EntityPlaceEvent e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		Entity entity = e.getEntity();
+		if (!(entity instanceof LivingEntity)) {
+			return;
+		}
+
+		BlockEvents.onEntityBlockPlace(level, e.getPos(), e.getPlacedBlock(), (LivingEntity)entity, null);
+	}
+}
